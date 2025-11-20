8c66857f27d5acf7ff958fbebc6ad8a5812a05b3
Mar Roca <m.rocacugat@student.maastrichtuniversity.nl>
2025-11-20 08:16:32 +0100

    oopsie...
    (I feel like that's a bit bigger than just oopsie)

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,4 +1,7 @@
-!mkdir ~/.kaggle #create the .kaggle folder in your root directory
-!echo '{"username":"stefzeemering","key":"5b2e65fcf94d620429eee3e27e514fbe"}' > ~/.kaggle/kaggle.json #write kaggle API credentials to kaggle.json
-!chmod 600 ~/.kaggle/kaggle.json  # set permissions
-!kaggle datasets download -d {'stefzeemering/atrial-fibrillation-classification'} -p /content/kaggle/ --force
+mkdir ~/.kaggle #create the .kaggle folder in your root directory
+echo '{"username":"UNAME","key":"KEY"}' > ~/.kaggle/kaggle.json #write kaggle API credentials to kaggle.json
+chmod 600 ~/.kaggle/kaggle.json  # set permissions
+kaggle datasets download -d {'stefzeemering/atrial-fibrillation-classification'} -p /content/kaggle/ --force
+unzip atrial-fibrillation-classification.zip
+
+# /!\ MAKE SURE NOT TO LEAK THE CREDENTIALS
